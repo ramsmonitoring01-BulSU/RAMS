46667a2db13de5e606820a68461535489ff7b5a4
ramsmonitoring01-BulSU <rams.monitoring01@gmail.com>
2026-09-06 19:28:49 +0800

    Update GateDetails.jsx

--- src/components/GateDetails.jsx
+++ src/components/GateDetails.jsx
@@ -4,9 +4,9 @@ import VehicleClearance from './VehicleClearance';
 
 const DEFAULT_THRESHOLDS = {
     noWater: 2.0,       // <= 2 cm: No Water
-    passableAll: 25.0,  // 2.1 - 20 cm: Passable to all vehicles
-    notLight: 41.0      // 20.1 - 66 cm: Not passable to light vehicles
-    // > 66 cm: Impassable to all vehicles
+    passableAll: 25.0,  // 2.1 - 25 cm: Passable to all vehicles
+    notLight: 41.0      // 25.1 - 41 cm: Not passable to light vehicles
+    // > 41.1 cm: Impassable to all vehicles
 };
 
 const getGateStatusDetails = (level, status, thresholds = DEFAULT_THRESHOLDS) => {
